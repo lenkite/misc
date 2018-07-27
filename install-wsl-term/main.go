0f5242df2121d735e0fa85ab1c76c8e596cfa4a0
Tarun Elankath <dev@elankath.com>
2018-07-27 11:27:17 +0530

    install-wsl-term v1 done

--- install-wsl-term/main.go
+++ install-wsl-term/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -9,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"path/filepath"
 	"regexp"
 )
 
@@ -30,6 +32,10 @@ func main() {
 	downloadLink := downloadLinks[0]
 	localPath, err := downloadFile(downloadLink)
 	log.Println("(main) Downloaded", localPath)
+
+	targDir := os.Getenv("HOME") + "/sdk"
+	unzip(localPath, targDir)
+	log.Println("(main) Un-zipped into:", targDir)
 }
 
 // DownloadFile will download a url to a local file in the system temp
@@ -57,8 +63,63 @@ func downloadFile(downloadLink string) (localPath string, err error) {
 	return
 }
 
-func unzip(zipPath string, targetDir string) error {
+func unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			panic(err)
+		}
+	}()
 
+	os.MkdirAll(dest, 0755)
+
+	// Closure to address file descriptors issue with all the deferred .Close() methods
+	extractAndWriteFile := func(f *zip.File) error {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := rc.Close(); err != nil {
+				panic(err)
+			}
+		}()
+
+		path := filepath.Join(dest, f.Name)
+
+		if f.FileInfo().IsDir() {
+			os.MkdirAll(path, f.Mode())
+		} else {
+			os.MkdirAll(filepath.Dir(path), f.Mode())
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer func() {
+				if err := f.Close(); err != nil {
+					panic(err)
+				}
+			}()
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+
+	for _, f := range r.File {
+		err := extractAndWriteFile(f)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func getLatestDownloadLinks(owner string, repo string) ([]string, error) {
